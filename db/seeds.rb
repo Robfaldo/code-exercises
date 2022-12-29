92cf9015b8d1b8a379d6e8fa101b1e457c304907
Robert Faldo <robertfaldo@gmail.com>
2022-12-29 10:18:28 +0000

    add trail seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,40 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-trail = Trail.create!(name: "Test trail")
-Exercise.create!(
-  name: "Test exercise",
-  github_repo_url: "https://github.com/marley-coder/test-exercise",
-  trail: trail
-)
 
-trail = Trail.create!(name: "Rails routes")
-Exercise.create!(
-  name: "Creating a route",
-  github_repo_url: "https://github.com/marley-coder/creating-a-route",
-  trail: trail
-)
-Exercise.create!(
-  name: "Showing all books",
-  github_repo_url: "https://github.com/marley-coder/showing-all-books",
-  trail: trail
-)
+if Rails.env.development?
+  trail = Trail.create!(name: "Test trail")
+  Exercise.create!(
+    name: "Test exercise",
+    github_repo_url: "https://github.com/marley-coder/test-exercise",
+    trail: trail
+  )
 
-trail = Trail.create!(name: "Test doubles")
-Exercise.create!(
-  name: "Doing a double",
-  github_repo_url: "https://github.com/marley-coder/doing-a-double",
-  trail: trail
-)
+  trail = Trail.create!(name: "Rails routes")
+  Exercise.create!(
+    name: "Creating a route",
+    github_repo_url: "https://github.com/marley-coder/creating-a-route",
+    trail: trail
+  )
+  Exercise.create!(
+    name: "Showing all books",
+    github_repo_url: "https://github.com/marley-coder/showing-all-books",
+    trail: trail
+  )
+
+  trail = Trail.create!(name: "Test doubles")
+  Exercise.create!(
+    name: "Doing a double",
+    github_repo_url: "https://github.com/marley-coder/doing-a-double",
+    trail: trail
+  )
+# else
+
+  trail = Trail.create!(name: "Phone number formatter")
+  Exercise.create!(
+    name: "exercise 1",
+    github_repo_url: "https://github.com/codelix-org/phone-number-exercise-1",
+    trail: trail
+  )
+end
+
