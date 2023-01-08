46a1edc3ba6082e71f948b4166b916221b6801df
Robert Faldo <robertfaldo@gmail.com>
2023-01-08 14:47:26 +0000

    remove name from exercise

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,7 +2,6 @@ class Exercise < ApplicationRecord
   belongs_to :trail
   has_many :attempts, dependent: :destroy
 
-  validates :name, presence: true
   validates :number, presence: true
   validates :github_repo_url, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_08_144425) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_144541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_08_144425) do
   end
 
   create_table "exercises", force: :cascade do |t|
-    t.string "name"
     t.string "github_repo_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
